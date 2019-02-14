6d190b66515b21c96f0d903217bfcae8360fe34f
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-02-13 22:25:29 -0500

    Added other checks

--- controls/el7_password_policy.rb
+++ controls/el7_password_policy.rb
@@ -49,20 +49,39 @@ control "Password_Creation_Requirement_Parameters_Using_pam_pwquality" do
 
 
 
-  describe file("/etc/pam.d/system-auth") do
-    its("content") { should match(/^\s*password\s+(?:required|requisite)\s+pam_pwquality.so\s+(?:\S+\s+)*try_first_pass(?:\s+\S+)*\s*$/) }
-  end
-  describe file("/etc/pam.d/system-auth") do
-    its("content") { should match(/^\s*password\s+(?:required|requisite)\s+pam_pwquality.so\s+(?:\S+\s+)*retry=[123](?:\s+\S+)*\s*$/) }
-  end
+  #describe file("/etc/pam.d/system-auth") do
+  #  its("content") { should match(/^\s*password\s+(?:required|requisite)\s+pam_pwquality.so\s+(?:\S+\s+)*try_first_pass(?:\s+\S+)*\s*$/) }
+  #end
+  #describe file("/etc/pam.d/system-auth") do
+  #  its("content") { should match(/^\s*password\s+(?:required|requisite)\s+pam_pwquality.so\s+(?:\S+\s+)*retry=[123](?:\s+\S+)*\s*$/) }
+  #end
 
   describe file("/etc/pam.d/system-auth") do
       its('content') { should match /^auth.*required.*pam_tally2.so.*onerr=fail.*deny=5$/ }
       its('content') { should match /^auth.*required.*pam_faildelay.so.*delay=2000000$/ }
+    #  its('content') { should match /^$/ }
+      its('content') { should match /^auth.*sufficient.*pam_fprintd.so$/ }
+      its('content') { should match /^auth.*requisite.*pam_succeed_if.so.*uid.*>=.*1000.*quiet_success$/ }
+      its('content') { should match /^auth.*sufficient.*pam_unix.so.*try_first_pass$/ }
+      its('content') { should match /^account.*sufficient.*pam_localuser.so$/ }
+      its('content') { should match /^account.*sufficient.*pam_succeed_if.so.*uid.*<.*1000.*quiet$/ }
+      its('content') { should match /^account.*required.*pam_permit.so$/ }
+      its('content') { should match /^password.*sufficient.*pam_unix.so.*sha512.*shadow.*nullok.*try_first_pass.*use_auth.*tok$/ }
       its('content') { should match /^password.*requisite.*pam_pwhistory.so.*remember=5.*use_authtok$/ }
   end
 
-
+  describe file("/etc/pam.d/password-auth") do
+      its('content') { should match /^auth.*required.*pam_tally2.so.*onerr=fail.*deny=5$/ }
+      its('content') { should match /^auth.*required.*pam_faildelay.so.*delay=2000000$/ }
+    #  its('content') { should match /^$/ }
+      its('content') { should match /^auth.*requisite.*pam_succeed_if.so.*uid.*>=.*1000.*quiet_success$/ }
+      its('content') { should match /^auth.*sufficient.*pam_unix.so.*try_first_pass$/ }
+      its('content') { should match /^account.*sufficient.*pam_localuser.so$/ }
+      its('content') { should match /^account.*sufficient.*pam_succeed_if.so.*uid.*<.*1000.*quiet$/ }
+      its('content') { should match /^account.*required.*pam_permit.so$/ }
+      its('content') { should match /^password.*sufficient.*pam_unix.so.*sha512.*shadow.*nullok.*try_first_pass.*use_auth.*tok$/ }
+      its('content') { should match /^password.*requisite.*pam_pwhistory.so.*remember=5.*use_authtok$/ }
+  end
 
 
 
